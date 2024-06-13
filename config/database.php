2e1d344f7093a05b569209854efaf8caed8ce9e1
doanduyvan <96610967+doanduyvan@users.noreply.github.com>
2024-06-13 13:29:13 +0700

    Delete config directory

--- config/database.php
+++
@@ -1,7 +0,0 @@
-<?php
-$conn = mysqli_connect('localhost', 'root', 'duyvan2001', 'workshopphp1_pet');
-    if($conn->connect_error){
-        echo "kết nối cơ sở dữ liệu không thành công";  
-    }
-
-?>
