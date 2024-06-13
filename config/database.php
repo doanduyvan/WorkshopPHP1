ca2f6d8a10f568c089853e800a9519b900f3cf64
doanduyvan <duyvanlee2001@gmail.com>
2024-06-13 12:55:09 +0700

    update gitignore

--- config/database.php
+++ config/database.php
@@ -1,5 +1,5 @@
 <?php
-$conn = mysqli_connect('localhost', 'root', 'phamngoctam', 'pet_shop');
+$conn = mysqli_connect('localhost', 'root', 'duyvan2001', 'workshopphp1_pet');
     if($conn->connect_error){
         echo "kết nối cơ sở dữ liệu không thành công";  
     }
